f64dd49c02fe65be39142dff72c06d90e68b4f2c
furqan934 <furqanmf234@gmail.com>
2025-06-20 12:45:49 +0500

    little changes in bubble sort

--- bubbleSort/code.cpp
+++ bubbleSort/code.cpp
@@ -2,14 +2,19 @@
 using namespace std;
 void bubbleSort(int arr[], int n){
     for ( int i = 0 ; i <n-1; i++)
-    {
+    {   
+        bool swaped = false;
         for (int j=0; j < n-i-1; j++){
             if(arr[j] > arr[j+1]){
                 swap(arr[j],arr[j+1]);
+                swaped = true;
             }
         }
+        if(!swaped){
+            cout <<"already sorted is the array!\n";
+            break;
+        }
     }
-
 }
 void traversing(int arr[],int n){
     for (int i =0 ; i < n; i ++){
